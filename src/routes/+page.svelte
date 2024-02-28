4e725bc9d24cb0868c002203083240f6bfd33885
Simone <xsimone97@hotmail.it>
2024-02-28 23:49:35 +0100

    added edit page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,22 @@
+<script lang="ts">
+	async function handleFiles() {
+		try {
+			// @ts-ignore
+			const directoryHandle = await window.showDirectoryPicker();
+			console.log('directoryHandle', directoryHandle);
+		} catch (error) {
+			console.error('Error opening directory', error);
+		}
+	}
+</script>
+
+<svelte:head>
+	<title>Hello world!</title>
+	<meta name="description" content="This is where the description goes for SEO" />
+</svelte:head>
+
 <h1 class="text-3xl font-bold underline">
-    Hello world!
+  Hello world!
 </h1>
+
+<button class="btn" on:click={handleFiles}>Button</button>
