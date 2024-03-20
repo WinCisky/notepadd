dcbf819e839740070a1edae220bc96a3cbac5c81
Simone <xsimone97@hotmail.it>
2024-03-20 22:03:25 +0100

    home update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,48 +16,36 @@
 		<div class="flex flex-col gap-6 max-w-lg">
 			<div class="flex items-center justify-center gap-2">
 				<Notepad class_name="w-24 h-24" />
-				<h1 class="text-5xl font-bold">
-					Notepadd
-				</h1>
+				<h1 class="text-5xl font-bold">Notepadd</h1>
 			</div>
-			<table class="table">
-				<tbody>
-					<tr>
-						<th>
-							<Folder class_name="w-6 h-6" />
-						</th>
-						<th>
-							Local
-						</th>
-						<td>
-							your file will stay only on your pc
-						</td>
-					</tr>
-					<tr>
-						<th>
-							<CreditCard class_name="w-6 h-6" />
-						</th>
-						<th>
-							Free
-						</th>
-						<td>
-							MIT license for personal and commercial use
-						</td>
-					</tr>
-					<tr>
-						<th>
-							<Heart class_name="w-6 h-6" />
-						</th>
-						<th>
-							Open source
-						</th>
-						<td>
-							for those brave enough to contribute
-						</td>
-					</tr>
-				</tbody>
-			</table>
-			<a href="{base}/editor" class="btn btn-outline mx-auto w-fit">Go to Editor</a>
+			{#if false}
+				<table class="table">
+					<tbody>
+						<tr>
+							<th>
+								<Folder class_name="w-6 h-6" />
+							</th>
+							<th> Local </th>
+							<td> your file will stay only on your pc </td>
+						</tr>
+						<tr>
+							<th>
+								<CreditCard class_name="w-6 h-6" />
+							</th>
+							<th> Free </th>
+							<td> MIT license for personal and commercial use </td>
+						</tr>
+						<tr>
+							<th>
+								<Heart class_name="w-6 h-6" />
+							</th>
+							<th> Open source </th>
+							<td> for those brave enough to contribute </td>
+						</tr>
+					</tbody>
+				</table>
+			{/if}
+			<a href="{base}/editor" class="btn btn-outline mx-auto w-fit">Open Editor</a>
 		</div>
 	</div>
 </div>
